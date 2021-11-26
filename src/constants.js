e7d0efc98a2e18d21af98f74e335ab0afc32c16b
AVX00 <93537515+AVX00@users.noreply.github.com>
2021-11-26 15:53:11 +0100

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,39 @@
+// Local Storage Key Values
+const LS_KEY = "cli-page-links";
+const LS_THEME_KEY = "cli-page-theme";
+const LS_ENGINE_KEY = "cli-page-engine";
+// File Constants
+const types = {
+    LINK: "link",
+    DIR: "directory",
+};
+const WEEK_DAYS = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+];
+// Defined Engines
+const ENGINES = {
+    google: "https://www.google.com/search?=",
+    ddg: "https://duckduckgo.com/?q=",
+    bing: "https://www.bing.com/search?q=",
+};
+// Defined Themes
+const THEMES = [
+    "dark",
+    "light",
+    "laserwave",
+    "nord",
+    "greyscale",
+    "dracula",
+    "nightowl",
+    "lightblue",
+    "pastel",
+    "vintage",
+    "sunset",
+    "forest",
+];
